962e79cc51cede23b3bed4c9b725db263f26ca70
Ingrid Marcano <mnonline2001@gmail.com>
2022-10-05 21:08:27 -0400

    se acomodaron las imagenes de el logo y el menu en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,76 +11,63 @@ import shoppingCart from '@icons/icon_shopping_cart.svg';
 import styles from '@styles/Header.module.scss';
 
 const Header = () => {
-    const [toggle, setToggle] = useState(false);
-    const [toggleOrders, setToggleOrders] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
 
-    const  { state } = useContext(AppContext); 
+  const { state } = useContext(AppContext);
 
-    const handleToggle = () => {
-        setToggle(!toggle);
-    }
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
-    const handleToggleOrders = () => {
-        setToggleOrders(!toggleOrders);
-    }
+  const handleToggleOrders = () => {
+    setToggleOrders(!toggleOrders);
+  };
 
-    return (
-        <nav className={styles.Nav}>
-            <Image src={`/${menu}`} alt="menu" className={styles.menu} 
-            width="100%"
-            height="100%" 
-            layout="responsive" 
-            />
-            <div className={styles['navbar-left']}>
-                <Link href="/">
-                    <Image src={`/${logo}`} alt="logo" className={styles['nav-logo']} 
-                    width="100%"
-                    height="100%" 
-                    layout="responsive"    
-                    />
-                </Link>
+  return (
+    <nav className={styles.Nav}>
+      <Image src={menu} alt="menu" className={styles.menu} width="23px" height="23px" />
+      <div className={styles['navbar-left']}>
+        <Link href="/">
+          <Image src={logo} alt="logo" className={styles['nav-logo']} width="100px" height="50px" />
+        </Link>
 
-                <ul>
-                    <li>
-                        <a href="/">All</a>
-                    </li>
-                    <li>
-                        <a href="/">Clothes</a>
-                    </li>
-                    <li>
-                        <a href="/">Electronics</a>
-                    </li>
-                    <li>
-                        <a href="/">Furnitures</a>
-                    </li>
-                    <li>
-                        <a href="/">Toys</a>
-                    </li>
-                    <li>
-                        <a href="/">Others</a>
-                    </li>
-                </ul>
-            </div>
-            <div className={styles['navbar-right']}>
-                <ul>
-                    <li className={styles['navbar-email']} onClick={handleToggle}>
-                        platzi@example.com
-                    </li>
-                    <li className={styles['navbar-shopping-cart']} onClick={handleToggleOrders}>
-                        <Image src={`/${shoppingCart}`} alt="shopping cart" 
-                        width="100%"
-                        height="100%" 
-                        layout="responsive" 
-                        />
-                        {state.cart.length > 0 ?  <div>{state.cart.length}</div> : null }
-                    </li>
-                </ul>
-            </div>
-            {toggle && <Menu />}
-            {toggleOrders && <MyOrder/>}
-        </nav>
-    );
-}
+        <ul>
+          <li>
+            <a href="/">All</a>
+          </li>
+          <li>
+            <a href="/">Clothes</a>
+          </li>
+          <li>
+            <a href="/">Electronics</a>
+          </li>
+          <li>
+            <a href="/">Furnitures</a>
+          </li>
+          <li>
+            <a href="/">Toys</a>
+          </li>
+          <li>
+            <a href="/">Others</a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles['navbar-right']}>
+        <ul>
+          <li className={styles['navbar-email']} onClick={handleToggle}>
+            platzi@example.com
+          </li>
+          <li className={styles['navbar-shopping-cart']} onClick={handleToggleOrders}>
+            <Image src={`/${shoppingCart}`} alt="shopping cart" width="100%" height="100%" layout="responsive" />
+            {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
+          </li>
+        </ul>
+      </div>
+      {toggle && <Menu />}
+      {toggleOrders && <MyOrder />}
+    </nav>
+  );
+};
 
 export default Header;
-
