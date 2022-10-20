e3dadf43aafec33b3944ba0014d519840ae3640c
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-10-20 09:41:56 -0400

    feat: adding if for avoid image error. Ref sn

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,35 +1,43 @@
-import React, { useContext } from 'react';
-import Image from 'next/image';
-import AppContext from '@context/AppContext';
-import addToCartImage from '@icons/bt_add_to_cart.svg';
-import styles from '@styles/ProductItem.module.scss';
+import React, { useContext } from 'react'
+import Image from 'next/image'
+import AppContext from '@context/AppContext'
+import addToCartImage from '@icons/bt_add_to_cart.svg'
+import styles from '@styles/ProductItem.module.scss'
 
 const ProductItem = ({ product }) => {
-  const { state, addToCart } = useContext(AppContext);
+  const { state, addToCart } = useContext(AppContext)
 
   const handleClick = (item) => {
-    console.log('in cart', state.cart.includes(item));
-    addToCart(item);
-  };
+    console.log('in cart', state.cart.includes(item))
+    addToCart(item)
+  }
+
+  if (['30A_00034.jpg', '31A_00035.jpg'].includes(product.images[0])) {
+    return (<div className={ styles.ProductItem }>
+      <h1>invalid image</h1>
+    </div>);
+  }
 
   return (
-    <div className={styles.ProductItem}>
-      {product.images[0] && <Image src={product.images[0]} width={240} height={240} alt={product.title} />}
-      <div className={styles['Produc-info']}>
+    <div className={ styles.ProductItem }>
+      { product.images[0] && <Image src={product.images[0]} width={240} height={240} alt={product.title} /> }
+      <div className={ styles['Produc-info'] }>
         <div>
-          <p>${product.price}</p>
-          <p>{product.title}</p>
+          <p>${ product.price }</p>
+          <p>{ product.title }</p>
         </div>
-        <figure className={styles['more-clickable-area']} onClick={() => handleClick(product)}>
-          {state.cart.includes(product) ? (
-            <Image className={`${styles.disabled} ${styles['add-to-cart-btn']}`} src={addedToCartImage} alt="added to cart" width="35px" height="35px" />
+        <figure className={ styles['more-clickable-area'] } onClick={ () => handleClick(product) }>
+          { state.cart.includes(product) ? (
+            <Image className={ `${ styles.disabled } ${ styles['add-to-cart-btn'] }` } src={ addedToCartImage }
+                   alt="added to cart" width="35px" height="35px" />
           ) : (
-            <Image className={styles['add-to-cart-btn pointer']} src={addToCartImage} alt="kkkk" width="35px" height="35px" />
-          )}
+            <Image className={ styles['add-to-cart-btn pointer'] } src={ addToCartImage } alt="kkkk" width="35px"
+                   height="35px" />
+          ) }
         </figure>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductItem;
+export default ProductItem
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,25 +1,22 @@
-import React,  { useEffect, useState} from 'react';
+import React from 'react'
 import ProductItem from '@components/ProductItem'
-import useGetProducts from '@hooks/useGetProducts';
-import styles from '@styles/ProductList.module.scss';
+import useGetProducts from '@hooks/useGetProducts'
+import styles from '@styles/ProductList.module.scss'
 
-
-const API = 'https://api.escuelajs.co/api/v1/products';
+const API = 'https://api.escuelajs.co/api/v1/products'
 
 const ProductList = () => {
-    const products = useGetProducts(API);
+  const products = useGetProducts(API)
 
-    
-    
-    return (
-      <section className={styles['main-container']}>
-          <div className={styles.ProuctList}>
-            {products.map((product) => (
-                <ProductItem  product={product} key={product.id} />
-            ))}
-          </div>
-      </section>
-    );
+  return (
+    <section className={ styles['main-container'] }>
+      <div className={ styles.ProuctList }>
+        { products.map((product) => (
+          <ProductItem product={ product } key={ product.id } />
+        )) }
+      </div>
+    </section>
+  )
 }
 
-export default ProductList;
+export default ProductList
